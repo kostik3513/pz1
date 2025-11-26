90310313fa6dca7a8cafed2e0d6953559054a7df
kostik3513 <kostik3513@gmail.com>
2025-11-26 15:21:56 +0200

    Add project files.

---
+++ pz1/pz1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+using namespace std;
+
+int main() {
+    system("color C3");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    string last_name;
+    cout << "Введіть прізвище: ";
+    getline(cin, last_name);
+
+    string first_name;
+    cout << "Введіть ім'я: ";
+    getline(cin, first_name);
+
+    int age;
+    cout << "Введіть вік: ";
+    cin >> age;
+
+    cin.ignore();
+
+    string city;
+    cout << "Введіть місце проживання: ";
+    getline(cin, city);
+
+    cout << "\n--- Результат ---\n";
+    cout << "Мене звати " << first_name << " " << last_name << endl;
+    cout << "Мені " << age << " років" << endl;
+    cout << "Я живу в " << city << endl;
+
+    return 0;
+}
